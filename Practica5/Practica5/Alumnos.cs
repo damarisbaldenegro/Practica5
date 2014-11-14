bb724c85a5370b512d88c46c1c2768618041d3b9
damaris Baldenegro <damarisbaldenegro@gmail.com>
2014-11-14 10:40:02 -0600

    Version 1.0

---
+++ Practica5/Practica5/Alumnos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Practica5
+{
+
+		public class Alumnos : MySQL
+	{
+		public Alumnos ()
+		{
+		}
+		
+		public void mostrarTodos(){
+			this.abrirConexion();
+            MySqlCommand myCommand = new MySqlCommand(this.querySelect(), 
+			                                          myConnection);
+		
+            MySqlDataReader myReader = myCommand.ExecuteReader();	
+	        while (myReader.Read()){
+	            string id = myReader["id"].ToString();
+	            string codigo = myReader["codigo"].ToString();
+	            string nombre = myReader["nombre"].ToString();
+	             string telefono = myReader["telefono"].ToString();
+	              string email = myReader["e-mail"].ToString();
+	              if(codigo.Length == 0 && nombre.Length == 0 && telefono.Length == 0 && email.Length == 0)
+						
+	            Console.WriteLine("ID: " + id +
+				                  " Código: " + codigo   + 
+				                  " Nombre: " + nombre   +
+				                  " Telefono" + telefono +
+				                  " E-mail"   + email    + "\n\n\n");
+	       }
+			
+            myReader.Close();
+			myReader = null;
+            myCommand.Dispose();
+			myCommand = null;
+			this.cerrarConexion();
+			
+		}
+		
+		public void insertarRegistroNuevo(int  codigo, string nombre,int telefono, string email){
+			this.abrirConexion();
+			string sql = "INSERT INTO `alumno` (`codigo`, `nombre`,'telefono','email) VALUES ('" + codigo + "', '" + nombre + "','"+ telefono +"','"+ email +"')";
+			Console.WriteLine("Registro Ingresado con exito");
+			this.ejecutarComando(sql);
+			
+				this.cerrarConexion();
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		private int ejecutarComando(string sql){
+			MySqlCommand myCommand = new MySqlCommand(sql,this.myConnection);
+			int afectadas = myCommand.ExecuteNonQuery();
+			myCommand.Dispose();
+			myCommand = null;
+			return afectadas;
+		}
+		
+		private string querySelect(){
+			return "SELECT * " +
+	           	"FROM alumno";
+		}
+	}
+}
+
+		
+		
+
---
+++ Practica5/Practica5/Principal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Practica5
+{
+	class Principal
+	{
+
+	
+		public static void Main (string[] args)
+		{
+		
+			Alumnos al = new Alumnos();
+			String opcion = "";
+			int registros= 0;
+			
+				
+		
+			
+				Console.WriteLine("INGRESE EL NUMERO DE OPCIÓN");
+				Console.WriteLine("ELIJA UNA OPCIÓN DEL MENU:");
+				Console.WriteLine("1.Agregar registro en la BD.");
+				Console.WriteLine("2.Consultar todos los registros.");
+				Console.WriteLine("3.Salir.");
+				
+				int op = Int32.Parse( Console.ReadLine() );
+				switch(op)
+						
+					{
+					case 1:
+						try{
+						int codigo = 0;
+						Console.WriteLine("dame el codigo del Alumno:");
+						codigo = int.Parse( Console.ReadLine() );
+						}catch (Exception e){
+								Console.WriteLine("*SE PEDÍA UN VALOR NUMÉRICO.");
+								Console.WriteLine(e.Message);}
+							
+								
+						Console.WriteLine("dame el nombre del Alumno:");
+						String nombre = Console.ReadLine();
+						int telefono = 0;
+						
+						Console.WriteLine("Ingrese el TELÉFONO del Alumno:");
+						telefono = Int32.Parse( Console.ReadLine() );
+						
+						Console.WriteLine("Ingrese el E-MAIL del Alumno:");
+						String email = Console.ReadLine();
+						
+						al.insertarRegistroNuevo(codigo,nombre,telefono,email);
+						codigo =0;
+						telefono = 0;
+						registros++;
+						break;	
+						
+						
+					case 2:
+					/*string bdVacia = al.mostrarTodos();
+						if(bdVacia || registros == 0)
+							Console.WriteLine("VACÍO" + "\r\n");
+						*/
+						break;
+					
+						
+					default:
+						Console.WriteLine("** ESA OPCION NO ESTA EN EL MENU **");
+						break;
+				}
+				
+			Console.ReadKey(true);	
+		}
+	}
+}
+
+
+
+		
